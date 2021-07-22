f0e3002e11d6f034bae7e3273bac74f9d21aad42
Bricks <tconckoff82@gmail.com>
2021-07-22 23:15:35 +0300

    Убрал лишнюю папку-прослойку и поправил ссылку в index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import ReactTemplate from "./ReactTemplate/Components/Page/Page";
+import Page from "./Components/Page/Page";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactTemplate />
+    <Page />
   </React.StrictMode>,
   document.getElementById("root")
 );
